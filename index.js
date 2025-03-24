3042d7cd8a458a431ce837532e4d9e593214cc94
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2025-03-24 23:49:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 const app = express();
 
 // const __filename = fileURLToPath (import.meta.url);
